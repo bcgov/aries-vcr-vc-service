7982ab4b022bb29176bbc0bda8e0e36df08498f3
PatStLouis <patrick.st-louis@opsecid.ca>
2024-11-26 17:31:46 +0000

    edit signed credential schema
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- schemas/signed_credential.py
+++ schemas/signed_credential.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, List
 from fastapi import Body
 from pydantic import BaseModel
 from schemas import Signed
@@ -11,10 +11,17 @@ class SignedCredential(Signed, BaseModel):
         list[str], Body(validation_alias="@context", serialization_alias="@context")
     ]
     id: str
-    type: list[str]
+    type: List[str] | str
     issuer: str | dict
     validFrom: str
-    validUntil: str
+    validUntil: str = None
     name: str | dict | None = None
     description: str | dict | None = None
     credentialSubject: dict
+    credentialSchema: List[dict] | dict = None
+    credentialStatus: List[dict] | dict = None
+    refreshService: List[dict] | dict = None
+    renderMethod: List[dict] | dict = None
+    termsOfUse: List[dict] | dict = None
+    relatedResources: List[dict] | dict = None
+    proof: List[dict] | dict = None
